1044a5260088822b92cf5dc9ffc54dbae0dfd5c2
Loi Tran <lvt15@my.fsu.edu>
2016-10-18 20:46:12 +0700

    got through milestone 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
 
-    if @user.save!
+    if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "User created successfully"
-      redirect_to root_path
+      redirect_to users_path
     else
+      flash[:error] = "Error: #{user.errors.full_messages.to_sentence}"
       render 'new'
     end
   end
@@ -18,4 +20,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
   root 'welcome#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
