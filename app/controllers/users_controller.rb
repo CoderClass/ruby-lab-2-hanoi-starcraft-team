6344e19cfcd0e63b8916fe3b1440863fa0f9f60d
Toan Ngo <xhunter9x@gmail.com>
2016-10-18 19:52:26 +0700

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new user_params
+
+    if @user.save!
+      flash[:success] = "User created successfully"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
   def index
     @users = User.all.shuffle
   end
